3845d907b7c9c81d5d445eaa634889c048e1d005
Maricruz1900 <utm22030695@utma.edu.mx>
2023-05-30 14:29:04 -0600

    feat:Función lista

---
+++ exercises.py
@@ -0,0 +1,10 @@
+#list
+foods = ["Pizza", "Hamburguer", "Chiness food", "Hotdog", "Tacos"]
+
+#add element 
+New_element = "wings"
+foods.append("wings")
+
+#print the element
+for food in foods:
+    print(food)
